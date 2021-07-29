e450027d0f3434bed1becdaa12b4886abc00852d
arundhati-c <81864366+arundhati-c@users.noreply.github.com>
2021-07-29 09:10:55 +0530

    state-before-demo

--- src/app/components/add-company/add-company.component.ts
+++ src/app/components/add-company/add-company.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ICompany } from 'src/app/model/company';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-company',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCompanyComponent implements OnInit {
 
-  constructor() { }
+  base_url = "http://localhost:8085/drives";
+  uploadForm? : FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) { }
+
+
+  company=new ICompany();
+
+  name = ''
+  website = ''
+  descriptiom = ''
 
   ngOnInit(): void {
+this.uploadForm = this.formBuilder.group({
+  companyName: [''],
+  companyWebsite:[''],
+  description:['']
+    });
+  }
+
+  onSubmit(){
+    // const formData = new FormData();
+    // formData.append('name', this.uploadForm.get('companyName').value);
+
+    // this.httpClient.post<any>(this.base_url, formData).subscribe(
+    //   (res) => console.log(res),
+    //   (err) => console.log(err)
+    // );
+
   }
 
 }
